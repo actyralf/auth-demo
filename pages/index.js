a50a925455ecb1050dbe800258c8b7b15a1b1582
Ralf Siewert <ralf.siewert@actyvyst.com>
2022-09-27 15:54:39 +0200

    integrate github oauth

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,19 @@ import styled from "styled-components";
 import axios from "axios";
 import Link from "next/link";
 import { Container } from "../components/container";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 const Students = ({ students }) => {
+  const { data: session } = useSession();
   return (
     <Container>
+      <p>{session ? session.user.email : <a onClick={signIn}>Anmelden</a>}</p>
       <h1>Otter unter sich</h1>
       <StyledList>
         {students.map((student) => {
           return (
             <li key={student._id}>
-              <Link href={`/students/${student._id}`}>
+              <Link href={`/students/${student._id}`} passHref>
                 <a>
                   {student.lastName}, {student.firstName}
                 </a>
