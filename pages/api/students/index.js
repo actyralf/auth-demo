c153b30f123e6f47ff04320f2a6199a064c5fd1a
Ralf Siewert <ralf.siewert@actyvyst.com>
2022-09-28 04:53:02 +0200

    finetuning

--- pages/api/students/index.js
+++ pages/api/students/index.js
@@ -4,7 +4,7 @@ import Student from "../_db/models/student";
 const handler = async (req, res) => {
   if (req.method === "GET") {
     try {
-      const students = await Student.find({}).exec();
+      const students = await Student.find({}).sort({ lastName: 1 }).exec();
       return res.status(200).json(
         students.map((student) => {
           const {
@@ -16,7 +16,7 @@ const handler = async (req, res) => {
         })
       );
     } catch (error) {
-      return res.status(500).send({ message: error.message });
+      return res.status(500).json({ message: error.message });
     }
   } else {
     res.status(405).json({ message: "method not allowed" });
