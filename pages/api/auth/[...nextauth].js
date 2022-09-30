37220e8662b82019f9cfefe4ca317a25c979808c
Ralf Siewert <ralf.siewert@actyvyst.com>
2022-09-30 11:39:43 +0200

    add fake auth for preview environment

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,13 +1,41 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
-export const authOptions = {
-  // Configure one or more authentication providers
-  providers: [
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const providers = [];
+if (process.env.VERCEL_ENV === "preview") {
+  providers.push(
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        if (
+          credentials.username === "test" &&
+          credentials.password === "test"
+        ) {
+          return {
+            name: "Test User",
+            email: "test@example.com",
+          };
+        } else {
+          return null;
+        }
+      },
+    })
+  );
+} else {
+  providers.push(
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
-    }),
-    // ...add more providers here
-  ],
+    })
+  );
+}
+
+export const authOptions = {
+  providers,
 };
 export default NextAuth(authOptions);
